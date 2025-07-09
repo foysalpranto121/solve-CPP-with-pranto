8b8e49ad9c93c6d4ea351c41f69a81cc10216cd9
foysalpranto121 <foysalpranto2002@gmail.com>
2025-07-10 00:56:24 +0600

    Add new file

---
+++ firstCode.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>// for clear input output
+
+//iostream is a input output stream ( a header file connected to #include)
+using namespace std;
+int main(){
+
+
+    cout << "hello world" <<  "  Pranto";
+    //cout  == standart output srtream ( show or print anything)
+
+    //return 0;
+    // it means sucessfully termination == keyword ofcourse
+
+    getch();
+    // for clear input output
+}
