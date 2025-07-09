4bd0c8e2cff501b87c15ae149eca3a5fede72683
foysalpranto121 <foysalpranto2002@gmail.com>
2025-07-10 04:30:54 +0600

    Add new file

---
+++ .vscode/1stclass.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+//cout <<"Hello world"<< endl;
+// addition 
+int a=5;
+int b=10;
+int sum=a+b;
+cout<< sum<<endl;
+
+
+
+//task
+int m=5;
+double n=19;
+double p= n/m;
+double x=int(n/m);
+// turn into int 
+cout<<p<<endl;
+cout<<x<<endl;
+
+//user input func
+// int y,z;
+
+// cin>> y >> z;
+// cout << y << " " << z << endl;
+int  q ,r,l;
+cin>>q>>r>>l;
+cout << q << " " << r << " "<< l<< endl;
+
+
+
+
+}
