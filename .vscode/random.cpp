cb2e01d69709a5bf08974ca17c81f8e03d1c5d34
foysalpranto121 <foysalpranto2002@gmail.com>
2025-09-01 21:49:17 +0600

    Add new file

---
+++ .vscode/random.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int32_t main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);
+
+int i,n,sum=0;
+cin>>n;
+for ( i = 1; i <=n; i++)
+{
+    sum=sum+i;
+
+}
+cout<<sum<<'\n';
+
+    return 0;
+}
